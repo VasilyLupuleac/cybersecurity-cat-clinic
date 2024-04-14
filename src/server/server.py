7f91761757ea7d1c29318a56a42f7f1d1a8d7b62
Anujrathee7 <anujrathee48@gmail.com>
2024-04-14 14:26:28 +0300

    Linking of registration and Login page

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from passwordStorage import DictPasswordStorage
+from passwordStorage import DictPasswordStorage,hash_password
 import os
 import cgi
 
@@ -12,9 +12,16 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Welcome to the clinic!')
         
-        elif page == 'messages':
-            self.send_response(403)
+        elif page == 'login':
+            self.send_response(200)
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            file_path = os.path.join(current_dir, 'login.html')
+
+            with open(file_path, 'rb') as file:
+                html_content = file.read()
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
+            self.wfile.write(html_content)
         
         elif page == 'register':
             self.send_response(200)
@@ -34,6 +41,7 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         page = self.path.split('/')[1]
+        print(f"Received POST request for page: {page}")
         if page == 'login':
             # TODO check login
             # TODO set headers
@@ -43,12 +51,30 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
         elif page == 'register':
             content_type, _ = cgi.parse_header(self.headers['Content-Type'])
+            print(f"Content-Type: {content_type}")
             if content_type == 'multipart/form-data':
                 form_data = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
                 username = form_data.getvalue('username')
                 password = form_data.getvalue('password')
                 confirm_pwd = form_data.getvalue('confirmPwd')
-                print(f'Username: {username}, Password: {password}, Confirm Password: {confirm_pwd}')
+                
+                print(f"Received registration data - Username: {username}, Password: {password}, Confirm Password: {confirm_pwd}")
+                
+                if str(password) == str(confirm_pwd):
+                    # TODO adding user to the password storage and checking if the user already exist
+
+                    
+                    print("User Added Successfully")
+                    
+                    # After successful user registration
+                    
+                    self.send_response(200)  # Found (redirect)
+                    self.send_header('Location', '/register?success=true')  # Redirect to registration page with success query parameter
+                    self.end_headers()
+
+                    #else:
+                    #    print("User Already exists")
+
             else:
                 self.send_response(400)  # Bad request
                 self.end_headers()
@@ -57,6 +83,7 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
         else:
             self.send_response(404)
             self.end_headers()
+
 
 
 userStorage = DictPasswordStorage()
